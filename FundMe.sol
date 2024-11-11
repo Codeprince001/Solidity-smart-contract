fd3704a1240bcfa004079e25c05a487fec2667eb
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-11 11:49:59 +0100

    importing price data from chainlink datafeed

--- FundMe.sol
+++ FundMe.sol
@@ -2,10 +2,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
 contract FundMe {
 
     uint256 public minimumUsd = 5;
     function fund() public payable{
         require(msg.value >= minimumUsd, "Enter minimum value");
+    }
+
+    function getPrice() public {
+        // 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    }
+    function getConvertionRate() public {}
+
+    function getVersion() public view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 }
