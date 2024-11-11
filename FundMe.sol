c01338453e73bd30056edbf0ba2bc6e4263c81fd
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-11 12:28:20 +0100

    get price from chainlink datafeed

--- FundMe.sol
+++ FundMe.sol
@@ -12,8 +12,12 @@ contract FundMe {
         require(msg.value >= minimumUsd, "Enter minimum value");
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256){
         // 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+        
     }
     function getConvertionRate() public {}
 
