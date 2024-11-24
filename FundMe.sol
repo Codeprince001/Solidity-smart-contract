8bb5e726108ba1a9c430c8f35b464fd666cb4ce6
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-24 15:42:48 +0100

    sending ether

--- FundMe.sol
+++ FundMe.sol
@@ -17,4 +17,22 @@ contract FundMe {
         funders.push(msg.sender);
         amountsFunded[msg.sender] = amountsFunded[msg.sender] + msg.value;
     }
+
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            amountsFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // actually withdraw fund:
+        // transfer
+        payable (msg.sender).transfer(address(this).balance);
+        // send
+        bool sendSuccess = payable (msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send Failed");
+        // call
+        (bool callSuccess, ) = payable (msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+
+    }
 }
